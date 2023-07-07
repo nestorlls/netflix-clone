dc1560739e4eacaf3f22a7d0ccbe90d94ab1551b
Paul Llanque <llanquepol@gmail.com>
2023-07-07 00:26:55 -0500

    add Movies page

---
+++ src/pages/Movies/Movies.jsx
@@ -0,0 +1,12 @@
+import NetflixLayout from '../../components/Layouts/NetflixLayout/NetflixLayout';
+import MoviesWrapper from './ui-Movies';
+
+const Movies = () => {
+  return (
+    <NetflixLayout>
+      <MoviesWrapper>Movies</MoviesWrapper>
+    </NetflixLayout>
+  );
+};
+
+export default Movies;
