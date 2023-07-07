7dc41334d0ea8a26bf041e6ad5659a815716e5bb
Paul Llanque <llanquepol@gmail.com>
2023-07-07 00:28:05 -0500

    update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login/Login';
 import Signup from './pages/Signup/Signup';
+import Home from './pages/Home/Home';
+import TvShow from './pages/TvShow/TvShow';
+import Movies from './pages/Movies/Movies';
+import MyList from './pages/MyList/MyList';
 
 const App = () => {
   return (
@@ -9,6 +13,10 @@ const App = () => {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/tv" element={<TvShow />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/mylist" element={<MyList />} />
         </Routes>
       </BrowserRouter>
     </>
