1247050ae6f9480bd8d3f02115e96b287fb744f0
Paul Llanque <llanquepol@gmail.com>
2023-07-07 00:27:11 -0500

    add Tvshow page

---
+++ src/pages/TvShow/TvShow.jsx
@@ -0,0 +1,12 @@
+import NetflixLayout from '../../components/Layouts/NetflixLayout/NetflixLayout';
+import TvShowWrapper from './ui-TvShow';
+
+const TvShow = () => {
+  return (
+    <NetflixLayout>
+      <TvShowWrapper>TvShow</TvShowWrapper>
+    </NetflixLayout>
+  );
+};
+
+export default TvShow;
---
+++ src/pages/TvShow/ui-TvShow.js
@@ -0,0 +1,5 @@
+import { styled } from 'styled-components';
+
+const TvShowWrapper = styled.div``;
+
+export default TvShowWrapper;
