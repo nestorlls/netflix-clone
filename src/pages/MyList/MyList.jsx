ffdc58f1fa17c9ef4c301415dc663c322e3a13f4
Paul Llanque <llanquepol@gmail.com>
2023-07-07 00:27:22 -0500

    add myList page

---
+++ src/pages/MyList/MyList.jsx
@@ -0,0 +1,12 @@
+import MyListWrapper from './ui-MyList';
+import NetflixLayout from '../../components/Layouts/NetflixLayout/NetflixLayout';
+
+const MyList = () => {
+  return (
+    <NetflixLayout>
+      <MyListWrapper>MyList</MyListWrapper>
+    </NetflixLayout>
+  );
+};
+
+export default MyList;
