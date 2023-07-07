afe11ff946e3ec7dbf854c5b96bbc1e972182e27
Paul Llanque <llanquepol@gmail.com>
2023-07-06 21:31:16 -0500

    update HeaderLogin

--- src/components/HeaderLogin/HeaderLogin.jsx
+++ src/components/HeaderLogin/HeaderLogin.jsx
@@ -1,15 +1,27 @@
-import { useState } from 'react';
 import logo from '../../assets/logo.png';
 import HeaderLoginWrapper from './ui-HeaderLogin';
+import { useLocation, useNavigate } from 'react-router-dom';
+
 const HeaderLogin = () => {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  const handleNavigation = () => {
+    navigate('/login');
+  };
+
   return (
     <HeaderLoginWrapper className="flex a-center j-between">
       <div className="logo">
         <img src={logo} alt="" />
       </div>
-      <button type="button" className="btn">
-        Login
-      </button>
+      {pathname === '/signup' && (
+        <div>
+          <button type="button" className="btn" onClick={handleNavigation}>
+            Login
+          </button>
+        </div>
+      )}
     </HeaderLoginWrapper>
   );
 };
