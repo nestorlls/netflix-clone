ec0052d577903bd7fe91c88a16c87c629983c187
Paul Llanque <llanquepol@gmail.com>
2023-07-06 21:35:48 -0500

    update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login/Login';
+import Signup from './pages/Signup/Signup';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
