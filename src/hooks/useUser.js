0545f8191d862cac9daa281720dc1b637a98a701
Paul Llanque <llanquepol@gmail.com>
2023-07-06 22:22:43 -0500

    add useUser hook

---
+++ src/hooks/useUser.js
@@ -0,0 +1,24 @@
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../utils/firebase-config';
+import { useEffect, useState } from 'react';
+
+const useUser = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const getUser = setTimeout(() => {
+      onAuthStateChanged(auth, (user) => {
+        if (!user) return;
+        else setUser(user);
+      });
+    });
+
+    return () => {
+      clearTimeout(getUser);
+    };
+  }, [user]);
+
+  return { user };
+};
+
+export default useUser;
