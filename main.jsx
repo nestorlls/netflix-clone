452c22bedb437b298e92a46dba72469292ba74f0
Paul Llanque <llanquepol@gmail.com>
2023-07-07 23:36:59 -0500

    update App

--- main.jsx
+++ main.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './src/App';
 import './src/index.css';
+import { Provider } from 'react-redux';
+import { store } from './src/app/store';
 
 const root = createRoot(document.getElementById('app'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
