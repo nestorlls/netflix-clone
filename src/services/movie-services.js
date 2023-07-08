8bb78f1a6f64dda8b5ab37a5b55ee67d6445faa6
Paul Llanque <llanquepol@gmail.com>
2023-07-07 23:35:18 -0500

    add movie services

---
+++ src/services/movie-services.js
@@ -0,0 +1,75 @@
+import axios from 'axios';
+import { API_KEY, TMBD_BASE_URL } from '../utils/constanst';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+const getRawData = async (url, genres) => {
+  const MovieArray = [];
+  for (let page = 1; page <= 5; page++) {
+    const { data } = await axios.get(`${url}&page=${page}`);
+    const movies = await data.results;
+    createArrayFromRawData(movies, MovieArray, genres);
+  }
+  return MovieArray;
+};
+
+const createArrayFromRawData = (movies, MovieArray, genres) => {
+  for (const movie of movies) {
+    const movieGenres = [];
+    for (const id of movie.genre_ids) {
+      const nameGenre = genres.find((genre) => genre.id === id);
+
+      if (nameGenre) {
+        movieGenres.push(nameGenre.name);
+      }
+    }
+
+    if (movie.backdrop_path) {
+      MovieArray.push({
+        id: movie.id,
+        name: movie?.original_name || movie.original_title,
+        image: movie.backdrop_path,
+        genres: movieGenres.slice(0, 3),
+      });
+    }
+  }
+};
+
+const apifetchData = createAsyncThunk(
+  'netflix/data',
+  async ({ type }, thunkApi) => {
+    const { netflix } = thunkApi.getState();
+    const genres = netflix.genres;
+
+    const url = `${TMBD_BASE_URL}/trending/${type}/week?api_key=${API_KEY}&page=3`;
+
+    const data = await getRawData(url, genres);
+    return data;
+  }
+);
+
+const apiFetchGenres = createAsyncThunk('netflix/genres', async () => {
+  const url = `${TMBD_BASE_URL}/genre/movie/list?api_key=${API_KEY}`;
+  const { data } = await axios.get(url);
+  return data.genres;
+});
+
+const apiFetchMovieByGenre = createAsyncThunk(
+  'netflix/genres',
+  async ({ type, genre }) => {
+    const movieArrayByGenre = [];
+
+    for (let page = 1; page <= 5; page++) {
+      const response = axios.get(
+        `${TMBD_BASE_URL}/discover/${type}?api_key=${API_KEY}&with_genres=${genre}`
+      );
+
+      const { data } = await response;
+      const { results } = await data;
+      movieArrayByGenre.push(...results);
+    }
+
+    return movieArrayByGenre;
+  }
+);
+
+export { apifetchData, apiFetchGenres, apiFetchMovieByGenre };
