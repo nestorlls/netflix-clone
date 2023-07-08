5257617f2dcd7f2ad07912f0842885cbce6a377f
Paul Llanque <llanquepol@gmail.com>
2023-07-07 23:36:45 -0500

    update Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import HomeWrapper from './ui-Home';
 import NetflixLayout from '../../components/Layouts/NetflixLayout/NetflixLayout';
 import bgImg from '../../assets/home.jpg';
+import movielogo from '../../assets/homeTitle.webp';
+import Slider from '../../components/Slider/Slider';
+import {
+  apiFetchGenres,
+  apiFetchMovieByGenre,
+  apifetchData,
+} from '../../services/movie-services';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Home = () => {
+  const isLoaded = useSelector((state) => state.netflix.genresLoaded);
+  const genres = useSelector((state) => state.netflix.genres);
+  const movies = useSelector((state) => state.netflix.movies);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(apiFetchGenres());
+  }, []);
+
+  useEffect(() => {
+    const getMovie = setTimeout(async () => {
+      dispatch(apifetchData({ type: 'all' }));
+    }, 500);
+
+    return () => {
+      clearTimeout(getMovie);
+    };
+  }, [isLoaded]);
+
+  console.log(movies);
+
   return (
     <NetflixLayout>
       <HomeWrapper>
-        <img src={bgImg} alt="" />
+        <div className="bgImageMovie"></div>
+        <div className="home-container flex a-center">
+          <div className="home-content flex column">
+            <img src={movielogo} alt="movie logo" />
+            <div className="buttons flex">
+              <button>Play</button>
+              <button>More Info</button>
+            </div>
+          </div>
+        </div>
+        <Slider movies={movies} />
       </HomeWrapper>
     </NetflixLayout>
   );
--- src/pages/Home/ui-Home.js
+++ src/pages/Home/ui-Home.js
@@ -1,5 +1,63 @@
 import { styled } from 'styled-components';
+import bgImgMovie from '../../assets/home.jpg';
 
-const HomeWrapper = styled.div``;
+const HomeWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+
+  .bgImageMovie {
+    height: 70vh;
+    width: 100%;
+    background-image: url(${bgImgMovie});
+    background-size: cover;
+    background-repeat: no-repeat;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+    opacity: 0.5;
+  }
+
+  .home-container {
+    position: relative;
+    height: 60vh;
+
+    .home-content {
+      align-items: self-start;
+      gap: 2rem;
+
+      :is(img, .buttons) {
+        margin-left: 2rem;
+      }
+
+      .buttons {
+        gap: 2rem;
+
+        button {
+          font-size: 1.4rem;
+          gap: 1rem;
+          border-radius: 0.5rem;
+          padding: 0.5rem 2rem;
+          border: none;
+          cursor: pointer;
+          transition: 0.3s ease-in-out;
+
+          &:hover {
+            opacity: 0.8;
+          }
+
+          &:nth-of-type(2) {
+            background-color: rgba(109, 109, 110, 0.7);
+            color: white;
+
+            svg {
+              font-size: 1.8rem;
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default HomeWrapper;
