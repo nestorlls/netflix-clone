20174d8d065ed02e3ce2068b55d2628b13cb4b00
Paul Llanque <llanquepol@gmail.com>
2023-07-08 17:27:01 -0500

    update Netflix layout

--- src/components/Layouts/NetflixLayout/NetflixLayout.jsx
+++ src/components/Layouts/NetflixLayout/NetflixLayout.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import NetflixLayoutWrapper from './ui-NetflixLayout';
 import HeaderNetflix from '../../HeaderNetflix/HeaderNetflix';
+import { useNavigate } from 'react-router-dom';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../../../utils/firebase-config';
 
 const NetflixLayout = ({ children }) => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        navigate('/login');
+      }
+    });
+  }, []);
+
   return (
     <NetflixLayoutWrapper>
       <HeaderNetflix />
