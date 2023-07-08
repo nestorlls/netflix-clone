bf57ca5c51f3c11a560c14a3387a4012d718d169
Paul Llanque <llanquepol@gmail.com>
2023-07-07 23:33:01 -0500

    add constants config

---
+++ src/utils/constanst.js
@@ -0,0 +1,5 @@
+const TMBD_BASE_URL = 'https://api.themoviedb.org/3';
+const TMBD_IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
+const API_KEY = import.meta.env.VITE_MOVIE_API_KEY;
+
+export { TMBD_BASE_URL, API_KEY, TMBD_IMAGE_URL };
