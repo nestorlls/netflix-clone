ce2a2ff6ea86ca097111bbf3e2ca8a17c76f3987
Paul Llanque <llanquepol@gmail.com>
2023-07-08 19:03:53 -0500

    update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,31 @@
+import { Suspense, lazy } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from './pages/Login/Login';
-import Signup from './pages/Signup/Signup';
-import Home from './pages/Home/Home';
-import TvShow from './pages/TvShow/TvShow';
-import Movies from './pages/Movies/Movies';
-import MyList from './pages/MyList/MyList';
+const Loading = lazy(() => import('./components/Loading/Loading'));
+const Login = lazy(() => import('./pages/Login/Login'));
+const Signup = lazy(() => import('./pages/Signup/Signup'));
+const Home = lazy(() => import('./pages/Home/Home'));
+const TvShow = lazy(() => import('./pages/TvShow/TvShow'));
+const Movies = lazy(() => import('./pages/Movies/Movies'));
+const MyList = lazy(() => import('./pages/MyList/MyList'));
+const Player = lazy(() => import('./pages/Player/Player'));
+const NotFound404 = lazy(() => import('./pages/NotFound404/NotFound404'));
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/tv" element={<TvShow />} />
-          <Route path="/movies" element={<Movies />} />
-          <Route path="/mylist" element={<MyList />} />
-        </Routes>
+        <Suspense fallback={<Loading />}>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/player" element={<Player />} />
+            <Route path="/tv" element={<TvShow />} />
+            <Route path="/movies" element={<Movies />} />
+            <Route path="/mylist" element={<MyList />} />
+            <Route path="*" element={<NotFound404 />} />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </>
   );
