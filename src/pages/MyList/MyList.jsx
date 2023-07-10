82e17b56a722d706a8c01494fb80c77917074f5a
Paul Llanque <llanquepol@gmail.com>
2023-07-10 11:50:14 -0500

    update Mylist page

--- src/pages/MyList/MyList.jsx
+++ src/pages/MyList/MyList.jsx
@@ -1,10 +1,38 @@
 import MyListWrapper from './ui-MyList';
 import NetflixLayout from '../../components/Layouts/NetflixLayout/NetflixLayout';
+import { onAuthStateChanged } from 'firebase/auth';
+import { useEffect, useState } from 'react';
+import { auth } from '../../utils/firebase-config';
+import getMyList from '../../services/myList-services';
+import { useDispatch, useSelector } from 'react-redux';
+import Card from '../../components/Card/Card';
 
 const MyList = () => {
+  const [email, setEmail] = useState(null);
+  const movies = useSelector((state) => state.netflix.myList);
+  const dispatch = useDispatch();
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setEmail(user.email);
+    }
+  });
+
+  useEffect(() => {
+    if (!email) return;
+    dispatch(getMyList(email));
+  }, [email]);
+
   return (
     <NetflixLayout>
-      <MyListWrapper>MyList</MyListWrapper>
+      <MyListWrapper>
+        <h1 className="mylist-title">My List</h1>
+        <div className="mylist-container flex a-center">
+          {movies.map((movie, index) => (
+            <Card movie={movie} isLiked key={index} />
+          ))}
+        </div>
+      </MyListWrapper>
     </NetflixLayout>
   );
 };
--- src/pages/MyList/ui-MyList.js
+++ src/pages/MyList/ui-MyList.js
@@ -1,5 +1,16 @@
 import { styled } from 'styled-components';
 
-const MyListWrapper = styled.div``;
+const MyListWrapper = styled.div`
+  margin-top: 8rem;
+
+  .mylist-title {
+    margin-bottom: 4rem;
+  }
+
+  .mylist-container {
+    gap: 1rem;
+    flex-wrap: wrap;
+  }
+`;
 
 export default MyListWrapper;
