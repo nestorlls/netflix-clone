c0f8efa0d2bd7b22dfb19e62358bec6de91481dd
Paul Llanque <llanquepol@gmail.com>
2023-07-10 10:51:10 -0500

    update contants

--- src/utils/constanst.js
+++ src/utils/constanst.js
@@ -1,5 +1,6 @@
 const TMBD_BASE_URL = 'https://api.themoviedb.org/3';
 const TMBD_IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
 const API_KEY = import.meta.env.VITE_MOVIE_API_KEY;
+const NETFLIX_URL_API = 'http://localhost:5000';
 
-export { TMBD_BASE_URL, API_KEY, TMBD_IMAGE_URL };
+export { TMBD_BASE_URL, API_KEY, TMBD_IMAGE_URL, NETFLIX_URL_API };
